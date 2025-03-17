face70c122e4fc3b7c76739d246c0408c2f25324
hfhbd <22521688+hfhbd@users.noreply.github.com>
2025-03-17 10:06:46 +0100

    Refactor build

---
+++ gradle/build-logic/src/main/kotlin/kotlinMPPSetup.gradle.kts
@@ -0,0 +1,60 @@
+
+kotlin {
+    jvmToolchain(8)
+
+    explicitApi()
+    compilerOptions {
+        allWarningsAsErrors.set(true)
+        freeCompilerArgs.add("-Xexpect-actual-classes")
+        extraWarnings.set(true)
+    }
+
+    jvm()
+
+    js {
+        nodejs()
+    }
+
+    wasmJs {
+        nodejs()
+    }
+    wasmWasi {
+        nodejs()
+    }
+
+    // tier 1
+    macosX64()
+    macosArm64()
+    iosSimulatorArm64()
+    iosX64()
+    iosArm64()
+
+    // tier 2
+    linuxX64()
+    linuxArm64()
+    watchosSimulatorArm64()
+    watchosX64()
+    watchosArm32()
+    watchosArm64()
+    tvosSimulatorArm64()
+    tvosX64()
+    tvosArm64()
+
+    // tier 3
+    androidNativeArm32()
+    androidNativeArm64()
+    androidNativeX86()
+    androidNativeX64()
+    mingwX64()
+    watchosDeviceArm64()
+}
+
+val emptyJar by tasks.registering(Jar::class) { }
+
+publishing {
+    publications.withType<MavenPublication>().configureEach {
+        artifact(emptyJar) {
+            classifier = "javadoc"
+        }
+    }
+}
